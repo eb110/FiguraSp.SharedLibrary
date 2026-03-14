3c69a1a0ddf725ed55bd0948d7137b45197c266d
Wladyslaw Figura <wfigura@op.pl>
2026-03-14 15:10:52 +0000

    init

---
+++ FiguraSp.SharedLibrary.Solution/FiguraSp.SharedLibrary/DependencyInjection/SharedService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FiguraSp.SharedLibrary.DependencyInjection
+{
+    public static class SharedService
+    {
+        public static IServiceCollection AddSharedServies<TContext>
+        (this IServiceCollection services, IConfiguration config, string fileName) where TContext : DbContext
+        {
+            SqlServerService.AddSqlServerService<TContext>(services, config);
+
+            return services;
+        }
+    }
+}
---
+++ FiguraSp.SharedLibrary.Solution/FiguraSp.SharedLibrary/DependencyInjection/SqlSharedService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FiguraSp.SharedLibrary.DependencyInjection
+{
+    public static class SqlServerService
+    {
+        public static IServiceCollection AddSqlServerService<TContext>(this IServiceCollection services, IConfiguration config) where TContext : DbContext
+        {
+            // Add generic db context
+            services.AddDbContext<TContext>(option => option.UseSqlServer(
+                config.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+    }
+}
