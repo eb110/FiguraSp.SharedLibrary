bd8a7c3e4fa2b8b8e5788005cb5273d9568a2376
Wladyslaw Figura <wfigura@op.pl>
2026-03-14 15:32:43 +0000

    refactor

--- FiguraSp.SharedLibrary.Solution/FiguraSp.SharedLibrary/DependencyInjection/SharedService.cs
+++ FiguraSp.SharedLibrary.Solution/FiguraSp.SharedLibrary/DependencyInjection/SharedService.cs
@@ -7,7 +7,7 @@ namespace FiguraSp.SharedLibrary.DependencyInjection
     public static class SharedService
     {
         public static IServiceCollection AddSharedServies<TContext>
-        (this IServiceCollection services, IConfiguration config, string fileName) where TContext : DbContext
+        (this IServiceCollection services, IConfiguration config) where TContext : DbContext
         {
             SqlServerService.AddSqlServerService<TContext>(services, config);
 
